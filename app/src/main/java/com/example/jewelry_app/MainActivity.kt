024dfde0a73d65014412193e82d1f276a7fb2639
Marc Edison Escosio <marcedisonescosio@gmail.com>
2025-02-09 16:59:43 +0800

    Create simple android application

--- app/src/main/java/com/example/jewelry_app/MainActivity.kt
+++ app/src/main/java/com/example/jewelry_app/MainActivity.kt
@@ -1,16 +1,41 @@
 package com.example.jewelry_app
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput: EditText
+    lateinit var passwordInput: EditText
+    lateinit var loginBtn: Button
+    lateinit var forgotPassword: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Find UI elements here, outside of the ViewCompat listener
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+        forgotPassword = findViewById(R.id.forgot_password)
+
+        // Set the click listener for the login button here, outside of the ViewCompat listener
+        loginBtn.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Test Credentials", "Username : $username, Password : $password")
+        }
+
+        // Set the ViewCompat listener here, after finding the UI elements
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
